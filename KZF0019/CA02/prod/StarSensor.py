4af83b9a4163d8d31e2e560fdafd75b38206989b
KevinFAN <fanfankuankuan@live.com>
2015-10-28 13:02:54 -0500

    Red Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -137,7 +137,7 @@ class StarSensor(object):
                 #return hex(float(minbr))         
             except:
                 pass
-        #En.Environment.incrementTime(40)
+        self.env.incrementTime(40)
     
     def getSensorPosition(self):
         rightAscension =  math.pi / 2 + self.env.getTime() * 2 * math.pi / self.env.getRotationalPeriod()
