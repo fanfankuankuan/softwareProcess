6131d900abeae0c4944c98efbf9d386c68a582fd
KevinFAN <fanfankuankuan@live.com>
2015-10-29 14:20:26 -0500

    Red Light |

--- KZF0019/CA03/sandbox/getxml.py
+++ KZF0019/CA03/sandbox/getxml.py
@@ -3,12 +3,12 @@ Created on Oct 27, 2015
 
 @author: KevinFAN
 '''
-import  xml.dom.minidom
+import  xml.etree.cElementTree as etr
 
-dom = xml.dom.minidom.parse('abc.xml')
-root = dom.documentElement
+dom = etr.parse('abc.xml')
+root = dom.getroot()
+list1 = []
+for i in range(0,len(root)):
+    list1.append(root[i].text)
+print list1
 
-print dom
-print root.nodeName
-print root.nodeValue
-print root.nodeType
