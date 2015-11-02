abb2290a6f25d5b2fe2d44b6e916aadd54c4c228
KevinFAN <fanfankuankuan@live.com>
2015-11-02 13:05:44 -0600

    Green Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -73,7 +73,7 @@ class StarSensor(object):
                 yMaxnum = float(declination+self.fieldView/2)       #calculate the Max number in the y axle
                 yMinnum = float(declination-self.fieldView/2)       #calculate the Min number in the y axle
                 
-                with open("CA02StarTestChart.txt") as f:
+                with open(self.starFile) as f:
                     j = 0
                     content = f.read()
                     for i in content.splitlines():
