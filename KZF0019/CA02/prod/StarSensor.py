83c4b1c38628774ba6d81ecd24a0105f94649c37
KevinFAN <fanfankuankuan@live.com>
2015-11-02 22:30:51 -0600

    Green Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -65,7 +65,7 @@ class StarSensor(object):
             rightAscension =  math.pi / 2 + self.env.getTime() * 2 * math.pi / (86164.1*1000000)
             declination =  self.env.getTime() * 2 * math.pi / self.env.getRotationalPeriod()
             try:
-                while rightAscension  >= 2 * math.pi:
+                while rightAscension - self.fieldView/2 >= 2 * math.pi:
                     rightAscension = rightAscension - 2*math.pi
                 while declination - self.fieldView/2 >= math.pi:
                     declination = declination - 2*math.pi
