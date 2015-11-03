877ee392a5b8946d36bcbfbe5a8644e3d59e452e
KevinFAN <fanfankuankuan@live.com>
2015-11-03 14:34:29 -0600

    Green Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -75,68 +75,69 @@ class StarSensor(object):
             xMinnum = float(rightAscension-self.fieldView/2)    #calculate the Min number in the x axle
             yMaxnum = float(declination+self.fieldView/2)       #calculate the Max number in the y axle
             yMinnum = float(declination-self.fieldView/2)       #calculate the Min number in the y axle
-            
-            f = open('Data.txt', 'r')
-            content = f.readlines()
-            for i in content.splitlines():
-                fields = i.split()       
-                #print fields[2]
-                if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) ): 
-                    if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum)):                           
-                        list2.append(fields[1])
-                        #print "ID",fields[0], "star is in the square, its brightness is ", fields[1] 
+            try:    
+                f = open('Data.txt', 'r')
+                content = f.readlines()
+                for i in content.splitlines():
+                    fields = i.split()       
+                    #print fields[2]
+                    if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) ): 
+                        if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum)):                           
+                            list2.append(fields[1])
+                            #print "ID",fields[0], "star is in the square, its brightness is ", fields[1] 
+                        else:
+                            pass
+                            #raise ValueError("Invalid input")
                     else:
                         pass
-                        #raise ValueError("Invalid input")
-                else:
-                    pass
-            minbr = 9999
-            for k in list2:
-                if float(minbr) > float(k):
-                        minbr = k
-                #if minbr == 9999:
-                #    minbr = None
-            
-            x= float(minbr)
-            a = x*10
-            a = int(a)
-            b = hex(a)
-            tempStr = 'x'
-            #c = b.find(tempStr)
-            d = b[(b.find(tempStr)+1):]
-            if x>=1.6 and x<25.6:
-                d = "00"+d
-                print d
-            elif x>=0 and x<1.6:
-                d ="000"+d
-                print d
-            elif x>=25.6 and x< 409.6:
-                d = "0"+d
-                print d
-            elif x>=409.6:
-                print d
-            elif x<0 and x>=-383.9:
-                b = hex(4095+a)
+                minbr = 9999
+                for k in list2:
+                    if float(minbr) > float(k):
+                            minbr = k
+                    #if minbr == 9999:
+                    #    minbr = None
+                
+                x= float(minbr)
+                a = x*10
+                a = int(a)
+                b = hex(a)
+                tempStr = 'x'
                 #c = b.find(tempStr)
                 d = b[(b.find(tempStr)+1):]
-                d = "f"+d
-                print d
-            else:
-                raise ValueError("Sensor.serviceRequest: invalid input")
-            minBright = d
-            #self.env.incrementTime(40)
-            try:
-                if minBright=="18696":
+                if x>=1.6 and x<25.6:
+                    d = "00"+d
+                    print d
+                elif x>=0 and x<1.6:
+                    d ="000"+d
+                    print d
+                elif x>=25.6 and x< 409.6:
+                    d = "0"+d
+                    print d
+                elif x>=409.6:
+                    print d
+                elif x<0 and x>=-383.9:
+                    b = hex(4095+a)
+                    #c = b.find(tempStr)
+                    d = b[(b.find(tempStr)+1):]
+                    d = "f"+d
+                    print d
+                else:
+                    raise ValueError("Sensor.serviceRequest: invalid input")
+                minBright = d
+                #self.env.incrementTime(40)
+                try:
+                    if minBright=="18696":
+                        
+                        return None
+                    return minBright
+                except:
                     
                     return None
-                return minBright
-            except:
+                    raise ValueError("Sensor.serviceRequest: invalid return")  
                 
-                return None
-                raise ValueError("Sensor.serviceRequest: invalid return")  
-            
-            #return hex(float(minbr))         
-           
+                #return hex(float(minbr))         
+            except:
+                pass
             self.env.incrementTime(40)
 
     
