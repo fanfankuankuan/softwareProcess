2bda11c2cf3e530a005f96e977e2f9015a468452
KevinFAN <fanfankuankuan@live.com>
2015-11-02 20:45:33 -0600

    Green Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -65,6 +65,10 @@ class StarSensor(object):
             rightAscension =  math.pi / 2 + self.env.getTime() * 2 * math.pi / (86164.1*1000000)
             declination =  self.env.getTime() * 2 * math.pi / self.env.getRotationalPeriod()
             try:
+                while rightAscension -self.fieldView/2 >= 2 * math.pi:
+                    rightAscension = rightAscension - 2*math.pi
+                while declination >= math.pi:
+                    declination = declination - 2*math.pi
                 xMaxnum = float(rightAscension+self.fieldView/2)    #calculate the Max number in the x axle  
                 xMinnum = float(rightAscension-self.fieldView/2)    #calculate the Min number in the x axle
                 yMaxnum = float(declination+self.fieldView/2)       #calculate the Max number in the y axle
@@ -76,8 +80,8 @@ class StarSensor(object):
                     for i in content.splitlines():
                         fields = i.split('\t')       
                         #print fields[2]
-                        if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) or (float(fields[2]) <= xMaxnum-2*math.pi and float(fields[2]) >= xMinnum - 2*math.pi) or (float(fields[2]) <= xMaxnum+2*math.pi and float(fields[2]) >= xMinnum+2*math.pi)): 
-                            if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum) or (float(fields[3]) <= yMaxnum - 2*math.pi and float(fields[3]) >= yMinnum -2*math.pi) or (float(fields[3]) <= yMaxnum + 2*math.pi and float(fields[3]) >= yMinnum + 2*math.pi)):
+                        if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) #or (float(fields[2]) <= xMaxnum-2*math.pi and float(fields[2]) >= xMinnum - 2*math.pi) or (float(fields[2]) <= xMaxnum+2*math.pi and float(fields[2]) >= xMinnum+2*math.pi)): 
+                            if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum) #or (float(fields[3]) <= yMaxnum - 2*math.pi and float(fields[3]) >= yMinnum -2*math.pi) or (float(fields[3]) <= yMaxnum + 2*math.pi and float(fields[3]) >= yMinnum + 2*math.pi)):
                                 j += 1
                                 list1.append(fields[0])       #add the value into list
                                 list2.append(fields[1])
