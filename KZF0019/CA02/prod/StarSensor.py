a4630139cee49d3e35f1f2726f1222aa3aaebda0
KevinFAN <fanfankuankuan@live.com>
2015-11-02 22:42:01 -0600

    Green Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -60,6 +60,7 @@ class StarSensor(object):
      
         if (self.env == None ):
             return None
+            print 'a'
         else:
             rightAscension =  math.pi / 2 + self.env.getTime() * 2 * math.pi / (86164.1*1000000)
             declination =  self.env.getTime() * 2 * math.pi / self.env.getRotationalPeriod()
@@ -83,7 +84,7 @@ class StarSensor(object):
                     if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) ): 
                         if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum)):                           
                             list2.append(fields[1])
-                            print "ID",fields[0], "star is in the square, its brightness is ", fields[1] 
+                            #print "ID",fields[0], "star is in the square, its brightness is ", fields[1] 
                         else:
                             pass
                             #raise ValueError("Invalid input")
