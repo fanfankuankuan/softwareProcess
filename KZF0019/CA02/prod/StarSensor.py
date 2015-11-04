ddc55f1554bc30d42c58065992b15c2ff7938e7a
KevinFAN <fanfankuankuan@live.com>
2015-11-04 11:01:11 -0600

    Green Light |

--- KZF0019/CA02/prod/StarSensor.py
+++ KZF0019/CA02/prod/StarSensor.py
@@ -19,33 +19,33 @@ class StarSensor(object):
         self.fieldView = fieldOfView
         
     def initializeSensor(self, starFile = None):
-        bright = []
+        starCount = 0
         if(starFile == None):
-            raise ValueError("StarSensor.initializeSensor:  ")
-        if(isinstance(starFile,str)):
-            try:
-                #global lines
-                f = file(starFile)      
-                self.lines = f.readlines() 
-                #print "lines:" , lines  
-                length = 0
-                for line in self.lines[0:]:
-                    length += 1
-                    data = line.split() 
-                
-                    try :
-                        float(data[0])
-                        float(data[1])
-                    except: 
-                        raise ValueError("invalid data")                       
-                    bright.append(data[1])
-                    id.append(data[0]) 
-                #self.f.close() 
-            except:
-                raise ValueError("StarSensor.initializeSensor:  ")      
-        else:
-            raise ValueError("StarSensor.initializeSensor:  ")
-        return length
+            raise ValueError("StarSensor.initializeSensor:  invalid file type")
+        if not(isinstance(starFile, types.StringType)):
+            raise ValueError("StarSensor.initializeSensor:  invalid file name")
+        if not isfile(starFile):
+            raise ValueError("StarSensor.initializeSensor:  no file exists by the specified file name")
+        try:
+            self.starFile = starFile
+            fr = open(starFile, 'r')
+            fw = open("Data.txt",'w')
+            self.lines = fr.readlines()
+            for line in self.lines[0:]:
+                #infors = line.split()
+                #for i in range(0,len(infors)):
+                #    infors[i] = (float)(infors[i])
+                #    print infors
+                #fw.write(line)
+                starCount += 1
+            #print starCount
+            return starCount
+        except Exception, ex:
+            print ex
+            raise ValueError("StarSensor.initializeSensor:  file contains invalid information")
+        #finally:
+        #    fw.close()
+        #    fr.close()
     def StarSensor(self,fieldview=None):
         self.fieldView = fieldview
         self.env.__init__()
@@ -80,64 +80,67 @@ class StarSensor(object):
             yMinnum = float(declination-self.fieldView/2)       #calculate the Min number in the y axle
             #print xMaxnum,xMinnum,yMaxnum,yMinnum
             
-        
-            for i in self.lines[0:]:
+            try:    
+                with open(self.starFile) as f:
                 
-                fields = i.split()   
-                print fields
-                if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) ): 
-                    if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum)):                           
-                        list2.append(fields[1])
-                    #print "ID",fields[0], "star is in the square, its brightness is ", fields[1] 
-                    else:
-                        pass
-                    #raise ValueError("Invalid input")
-                else:
-                    pass
+                    content = f.readlines()
+                    for i in content[0:]:
+                        
+                        fields = i.split()   
+                        print fields
+                        if ((float(fields[2]) <= xMaxnum and float(fields[2]) >= xMinnum) ): 
+                            if ((float(fields[3]) <= yMaxnum and float(fields[3]) >= yMinnum)):                           
+                                list2.append(fields[1])
+                            #print "ID",fields[0], "star is in the square, its brightness is ", fields[1] 
+                            else:
+                                pass
+                            #raise ValueError("Invalid input")
+                        else:
+                            pass
                 #print list2
-            minbr = 9999
-            for k in list2:
-                if float(minbr) > float(k):
-                        minbr = k
-                #if minbr == 9999:
-                #    minbr = None
-            
-            x= float(minbr)
-            a = x*10
-            a = int(a)
-            b = hex(a)
-            tempStr = 'x'
-            c = b.find(tempStr)
-            d = b[(b.find(tempStr)+1):]
-            if x>=1.6 and x<25.6:
-                d = "00"+d
-                print d
-            elif x>=0 and x<1.6:
-                d ="000"+d
-                print d
-            elif x>=25.6 and x<409.6:
-                d = "0"+d
-                print d
-            elif x>=409.6:
-                print d
-            elif x<0 and x>=-383.9:
-                b = hex(4095+a)
+                minbr = 9999
+                for k in list2:
+                    if float(minbr) > float(k):
+                            minbr = k
+                    #if minbr == 9999:
+                    #    minbr = None
+                
+                x= float(minbr)
+                a = x*10
+                a = int(a)
+                b = hex(a)
+                tempStr = 'x'
                 c = b.find(tempStr)
                 d = b[(b.find(tempStr)+1):]
-                d = "f"+d
-                print d
-            else:
-                raise ValueError("")
-            minBright = d
-            #self.env.incrementTime(40)
-            try:
-                if minBright=="18696":
+                if x>=1.6 and x<25.6:
+                    d = "00"+d
+                    print d
+                elif x>=0 and x<1.6:
+                    d ="000"+d
+                    print d
+                elif x>=25.6 and x<409.6:
+                    d = "0"+d
+                    print d
+                elif x>=409.6:
+                    print d
+                elif x<0 and x>=-383.9:
+                    b = hex(4095+a)
+                    c = b.find(tempStr)
+                    d = b[(b.find(tempStr)+1):]
+                    d = "f"+d
+                    print d
+                else:
+                    raise ValueError("")
+                minBright = d
+                #self.env.incrementTime(40)
+                try:
+                    if minBright=="18696":
+                        return None
+                    return minBright 
+                except:
+            
                     return None
-                return minBright 
-            except:
-        
-                return None
-                raise ValueError("Sensor.serviceRequest: invalid return")  
+                    raise ValueError("Sensor.serviceRequest: invalid return")  
                 
                 #return hex(float(minbr))         
             except:
