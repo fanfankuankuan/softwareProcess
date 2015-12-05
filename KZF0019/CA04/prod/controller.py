94e02a0d67c86a6f0fb680d1741158a538ab0dab
KevinFAN <fanfankuankuan@live.com>
2015-12-04 20:56:24 -0600

    Green Light |

--- KZF0019/CA04/prod/controller.py
+++ KZF0019/CA04/prod/controller.py
@@ -142,7 +142,7 @@ class controller(object):
                 for i in range(0,len(self.d)):
                     #while(int(simulatedTime + microseconds) > self.c[0] ):
                     if self.d[i] == "SolarCollector":
-                        r = Solar.serviceRequest(self.c[2])
+                        r = Solar.serviceRequest()
                         result.append(r)
                         simulatedTime += 40
                         mm.serviceRequest("Controller","SolarCollector","service")
