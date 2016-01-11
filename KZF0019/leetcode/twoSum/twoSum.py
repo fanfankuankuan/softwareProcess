e9a3994dab2cef8b553a99f5bed7f603a276fc49
KevinFAN <fanfankuankuan@live.com>
2016-01-10 23:12:51 -0600

    Green Light |

---
+++ KZF0019/leetcode/twoSum/twoSum.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 10, 2016
+
+@author: KevinFAN
+'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in len(nums):
+            for j in len(nums):
+                if i < j and nums[i] < nums[j]:
+                    if nums[i] + nums[j] == target:
+                        print i+1
+                        print j+1
+                        #return i+1,j+1
+
+#nu = [1,2,3,4]
+r = Solution()
+read = r.twoSum([1,2,3,4],4)
