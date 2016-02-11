2a88b9f5138325c5c60d1e40270bac995b43dc2a
KevinFAN <fanfankuankuan@live.com>
2016-02-11 13:25:54 -0600

    Green Light |

--- KZF0019/COMP 6740/test1/lab1.py
+++ KZF0019/COMP 6740/test1/lab1.py
@@ -4,6 +4,21 @@ Created on Feb 11, 2016
 @author: KevinFAN
 '''
 import math
+import random
+
 
 class lab1(object):
-    
+    def num(self):
+        for r in range(1,10,1):
+            for p in range(1,9,1):
+                Failure = 0
+                Success = 0
+                v = float(p /10)
+                while(Success<r):
+                    number = random.randrange(1,10,1)
+                    v2 = float(number/100)
+                    if v2 < v:
+                        Success = Success +1
+                    else:
+                        Failure = Failure + 1
+                print (Success+Failure)
