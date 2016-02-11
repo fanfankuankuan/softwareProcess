d59f9f401c618b814c308b5dfe43adf90eaa0aaf
KevinFAN <fanfankuankuan@live.com>
2016-02-11 14:31:15 -0600

    Green Light |

--- KZF0019/COMP 6740/test1/lab1.py
+++ KZF0019/COMP 6740/test1/lab1.py
@@ -10,30 +10,30 @@ import os
 list = []
 class lab1(object):
     def num(self):
+        file = open('lab1.txt','w')
+        for r in range(1,10,1):
+            for p in range(1,9,1):
+                for i in range(1,100,1):
+                    Failure = 0
+                    Success = 0
+    #                 print p
+                    v = float(p)/10
+    #                 print v
+                    while(Success<r):
+                        number = random.randrange(1,10,1)
+                        v2 = float(number)/10
+                        if v2 <=v:
+                            Success = Success +1
+                            #print Success
+                        else:
+                            Failure = Failure + 1
+#                     file.write(Failure)
+                    list.append(Failure)
+        
+#         file = open('lab1.txt','w')
         try:
-            file = open('lab1.txt','w')
-            for r in range(1,10,1):
-                for p in range(1,9,1):
-                    for i in range(1,100,1):
-                        Failure = 0
-                        Success = 0
-        #                 print p
-                        v = float(p)/10
-        #                 print v
-                        while(Success<r):
-                            number = random.randrange(1,10,1)
-                            v2 = float(number)/10
-                            if v2 <=v:
-                                Success = Success +1
-                                #print Success
-                            else:
-                                Failure = Failure + 1
-    #                     file.write(Failure)
-                        list.append(Failure)
-            
-    #         file = open('lab1.txt','w')
             file.write(list)
-            file.close()
+        file.close()
 
 
 
